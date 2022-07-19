0030f5ffc6d3c5df6714881f8a699a62c3de9205
kmj4034 <kkmj4034@gmail.com>
2022-07-19 22:48:26 +0900

    과제3 제출

---
+++ session/session10/react-session03/src/PageTwo.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import BlueBox from "./BlueBox";
+
+const pageTwo = () => {
+  return (
+    <>
+      <div>pageTwo</div>
+      <BlueBox title="버튼" btnColor="red" />
+    </>
+  );
+};
+
+export default pageTwo;
